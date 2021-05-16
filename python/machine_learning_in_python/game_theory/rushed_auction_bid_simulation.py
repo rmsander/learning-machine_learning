29c19574acec85c3b59557c6c45d24e4e4079548
Ryan Sander <rmsander@mit.edu>
2021-05-16 12:30:28 -0400

    Updated Auction Bidding To adjust for custom variable ranges, variable values, and length of the dutch auction bid

--- python/machine_learning_in_python/game_theory/rushed_auction_bid_simulation.py
+++ python/machine_learning_in_python/game_theory/rushed_auction_bid_simulation.py
@@ -18,6 +18,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Specify duration of the bid
+NUM_DAYS = 40
+
 
 def make_alpha_beta_bids_dict():
     """Function to loop over and create dictionary mapping
@@ -25,21 +28,71 @@ def make_alpha_beta_bids_dict():
     # Initialize dictionary
     alpha_beta_gamma_bids_dict = {}
 
+    # Specify if we should keep variables constant
+    print("For reach, please enter y (yes) or n (no).")
+    alpha_constant = input("Keep alpha constant? --> ")
+    beta_constant = input("Keep beta constant? --> ")
+    gamma_constant = input("Keep gamma constant? --> ")
+
     # Specify granularity for parameter sweeps
-    alpha_step_size = float(
-        input("Please specify a step size for alpha (e.g. 0.01): --> "))
-    beta_step_size = float(
-        input("Please specify a step size for beta (e.g. 0.1): --> "))
-    gamma_step_size = float\
-        (input("Please specify a step size for gamma (e.g. 0.1): --> "))
+    if alpha_constant == "n":
+        alpha_step_size = float(
+            input("Please specify a step size for alpha (e.g. 0.1): --> "))
+    elif alpha_constant == "y":
+        print("Since alpha constant, step size is not needed.")
+
+    if beta_constant == "n":
+        beta_step_size = float(
+            input("Please specify a step size for beta (e.g. 0.1): --> "))
+    elif beta_constant == "y":
+        print("Since beta constant, step size is not needed.")
+
+    if gamma_constant == "n":
+        gamma_step_size = float(
+            input("Please specify a step size for gamma (e.g. 0.1): --> "))
+    elif gamma_constant == "y":
+        print("Since gamma constant, step size is not needed.")
+
+    # Specify ranges for alpha, beta, and gamma parameters
+    if alpha_constant == "n":
+        alpha_range = eval(
+            input("Please provide a range for alpha in the form of [low, high] --> "))
+        alpha_low, alpha_high = alpha_range[0], alpha_range[1]
+        alphas = np.arange(alpha_low, alpha_high + alpha_step_size, alpha_step_size)
+    elif alpha_constant == "y":
+        alphas = np.array(
+            float(input("Please provide a constant value for alpha --> "))).reshape(1)
+
+    if beta_constant == "n":
+        beta_range = eval(
+            input("Please provide a range for beta in the form of [low, high] --> "))
+        beta_low, beta_high = beta_range[0], beta_range[1]
+        betas = np.arange(beta_low, beta_high + beta_step_size, beta_step_size)
+    elif beta_constant == "y":
+        betas = np.array(
+            float(input("Please provide a constant value for beta --> "))).reshape(1)
+
+    if gamma_constant == "n":
+        gamma_range = eval(
+            input("Please provide a range for gamma in the form of [low, high] --> "))
+        gamma_low, gamma_high = gamma_range[0], gamma_range[1]
+        gammas = np.arange(gamma_low, gamma_high + gamma_step_size, gamma_step_size)
+    elif gamma_constant == "y":
+        gammas = np.array(
+            float(input("Please provide a constant value for gamma --> "))).reshape(1)
 
     # Use granularity to get number in each parameter
-    num_alphas = int(alpha_step_size ** -1)  # Since range is [0, 1]
-    num_betas = int(beta_step_size ** -1)  # Since range is [0, 1]
-    num_gammas = 2 * int(gamma_step_size ** -1)  # Since range is [0, 2]
+    num_alphas = alphas.size
+    num_betas = betas.size
+    num_gammas = gammas.size
+    print("Dimensions of output array:")
+    print("Num Alphas: {}, Range: {}".format(num_alphas, alphas))
+    print("Num Betas: {}, Range: {}".format(num_betas, betas))
+    print("Num Gammas: {}".format(num_gammas, gammas))
 
     # Initialize 3D array and ranges of variables to loop over
-    alpha_beta_gamma_array = np.zeros((num_alphas, num_betas, num_gammas)).astype(np.uint32)
+    alpha_beta_gamma_array = np.zeros(
+        (num_alphas, num_betas, num_gammas)).astype(np.uint32)
 
     # Get total number of parameter combinations
     N = num_alphas * num_betas * num_gammas
@@ -48,19 +101,11 @@ def make_alpha_beta_bids_dict():
     point_array = np.zeros((N, 3))
     days_array = np.zeros(N).astype(np.uint32)
 
-    # Specify range arrays for each parameter
-    alpha_range = np.arange(alpha_step_size, 1 + alpha_step_size,
-                            step=alpha_step_size)
-    beta_range = np.arange(beta_step_size, 1 + beta_step_size,
-                           step=beta_step_size)
-    gamma_range = np.arange(beta_step_size, 2 + beta_step_size,
-                            step=beta_step_size)
-
     # Loop over alpha, beta, and gamma
     idx = 0
-    for a, alpha in enumerate(alpha_range):  # Loop over "rows"
-        for b, beta in enumerate(beta_range):  # Loop over "columns"
-            for g, gamma in enumerate(gamma_range):  # Loop over "page"
+    for a, alpha in enumerate(alphas):  # Loop over "rows"
+        for b, beta in enumerate(betas):  # Loop over "columns"
+            for g, gamma in enumerate(gammas):  # Loop over "page"
 
                 # Get the bid day for the specific parameter configuration
                 bid_day = loop_helper_analytic(alpha, beta, gamma)
@@ -105,7 +150,7 @@ def make_alpha_beta_bids_dict():
 
     # Display and return
     print("BID DICTIONARY: \n{}".format(alpha_beta_gamma_bids_dict))
-    print("BID ARRAY: \n{}".format(alpha_beta_gamma_array))
+    print("BID ARRAY: \n{}".format(np.squeeze(alpha_beta_gamma_array)))
     return alpha_beta_gamma_bids_dict, alpha_beta_gamma_array
 
 
@@ -129,14 +174,16 @@ def loop_helper_analytic(alpha, beta, gamma):
         values = []
 
         # Get column data (utilities into the future) for current_day
-        for day_forward in range(current_day, 11):
+        for day_forward in range(current_day, NUM_DAYS+1):
 
             # Calculate probability of not losing (weighted by gamma)
             prob_not_losing = np.prod(
-                [1-(1/(11-day) * gamma) for day in range(current_day, day_forward + 1)])
+                [1-(1/(NUM_DAYS+1-day) * gamma) for day in range(current_day, day_forward + 1)])
 
             # Calculate discounted utility with quasi-hyperbolic preferences
-            discount_factor = beta * (alpha ** (abs(day_forward-current_day)))
+            discount_factor = alpha ** (abs(day_forward-current_day))
+            if day_forward > current_day:  # Only factor in beta if next day
+                discount_factor *= beta
             discounted_value = day_forward * discount_factor
 
             # Discount return with probability of not losing (expected utility)
